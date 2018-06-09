47a6aa9c86d607ba74e7e2ee073e339a9f028f42
Konstantin <Konstantin.savov@yahoo.com>
2018-06-09 07:55:54 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ function addEventListeners() {
     $('#main-container').find('h3 > span').each((index, e) => {
         const element = $(e);
         const container = element.parent().parent().find('.article-content');
+        $(".article-content:visible").hide()
 
         element.click(() => {
             if (container.css('display') === 'none')    {
