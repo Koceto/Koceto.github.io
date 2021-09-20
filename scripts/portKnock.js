40394f000fa8db9e4ea83e868631270731b15997
Konstantin Savov <konstantin.savov@yahoo.com>
2021-09-20 19:18:10 +0300

    gotta love it

--- scripts/portKnock.js
+++ scripts/portKnock.js
@@ -34,49 +34,12 @@ async function portKnock(requestDelay = 2000) {
     return;
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  // const knockElement = document.getElementById("knock-element");
-=======
->>>>>>> parent of 3d47144 (Added query parameters for IP and Ports)
-=======
   const knockElement = document.getElementById("knock-element");
->>>>>>> parent of 61e845d (minor changes)
   const portsOrderedList = document.createElement("ol");
   const portsLogElement = log("User entered the following ports:");
 
   portsLogElement.appendChild(portsOrderedList);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  for (const port of ports) {    
-      const urlToKnock = `https://${userIp}:${port.trim()}/`;
-    log(`Calling port \'${port}\' - \'${urlToKnock}\'`, logType.info, portsOrderedList);
-
-    try {
-      await sendUDP(userIp, +port);
-    } catch (err) {
-      log(`Error: ${err}`, logType.error)
-    }
-    
-    // await new Promise((resolve) => {
-    //   const knocker = document.createElement("img");
-    //   knocker.src = urlToKnock;
-    //   knockElement.appendChild(knocker)
-      
-    //   setTimeout(() => {
-    //     resolve();
-    //   }, requestDelay);
-    // });
-=======
-  for (const port of ports) {
-    const fetchUrl = `https://${userIp}:${port}/`;
-
-    log(`Calling port \'${port}\' - \'${fetchUrl}\'`, logType.info, portsOrderedList);
-
-    await new Promise((resolve) => {
-      fetch(fetchUrl).catch();
-=======
   for (const port of ports) {
     const urlToKnock = `https://${userIp}:${port.trim()}/`;
 
@@ -86,16 +49,11 @@ async function portKnock(requestDelay = 2000) {
       const knocker = document.createElement("img");
       knocker.src = urlToKnock;
       knockElement.appendChild(knocker)
->>>>>>> parent of 61e845d (minor changes)
 
       setTimeout(() => {
         resolve();
       }, requestDelay);
     });
-<<<<<<< HEAD
->>>>>>> parent of 3d47144 (Added query parameters for IP and Ports)
-=======
->>>>>>> parent of 61e845d (minor changes)
   }
 
   log("Done!");
